17810c10e17d0df577c89625adacdc8a40702346
Yeongu <46391818+365creed@users.noreply.github.com>
2026-01-30 20:28:39 +0900

    Update engine.js

--- core/engine.js
+++ core/engine.js
@@ -1,1 +1,34 @@
+import { Layers } from "../render/layers.js";
+import { updateScene, drawScene } from "./state.js";
 
+export class Engine {
+  constructor(canvas){
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.width = 360;
+    this.height = 640;
+
+    this.resize();
+    Layers.init(this.width, this.height, this.ctx);
+
+    window.addEventListener("resize",()=>this.resize());
+  }
+
+  resize(){
+    const dpr = window.devicePixelRatio || 1;
+    this.canvas.width = this.width * dpr;
+    this.canvas.height = this.height * dpr;
+    this.canvas.style.width = this.width + "px";
+    this.canvas.style.height = this.height + "px";
+    this.ctx.setTransform(dpr,0,0,dpr,0,0);
+  }
+
+  start(){
+    const loop = ()=>{
+      updateScene();
+      drawScene();
+      requestAnimationFrame(loop);
+    };
+    loop();
+  }
+}
