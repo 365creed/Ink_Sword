c615c2b52d069194b12a834a6b72dcf59d02a138
Yeongu <46391818+365creed@users.noreply.github.com>
2026-09-21 17:50:26 +0900

    Update player.js

--- js/entities/player.js
+++ js/entities/player.js
@@ -1,81 +1,26 @@
 export class Player {
-  constructor(x,y){
+  constructor(ctx, x, y) {
+    this.ctx = ctx;
     this.x = x;
     this.y = y;
-
-    this.w = 40;
-    this.h = 60;
-
-    this.speed = 220;
-
-    this.hp = 100;
-    this.alive = true;
-
-    this.attackCooldown = 0;
-    this.attackRange = 60;
-    this.damage = 25;
-
-    this.keys = {};
-
-    window.addEventListener("keydown",(e)=>this.keys[e.key]=true);
-    window.addEventListener("keyup",(e)=>this.keys[e.key]=false);
+    this.width = 60;
+    this.height = 80;
   }
 
-  update(dt){
-    if(!this.alive) return;
+  update(dt) {}
 
-    // 이동
-    if(this.keys["ArrowRight"]) this.x += this.speed * dt;
-    if(this.keys["ArrowLeft"]) this.x -= this.speed * dt;
-
-    // 공격 쿨
-    if(this.attackCooldown > 0){
-      this.attackCooldown -= dt;
-    }
-
-    // 공격
-    if(this.keys[" "] && this.attackCooldown <= 0){
-      this.attackCooldown = 0.5;
-      this.isAttacking = true;
-      setTimeout(()=> this.isAttacking=false, 150);
-    }
-
-    if(this.hp <= 0){
-      this.alive = false;
-    }
-  }
-
-  draw(ctx){
-    ctx.fillStyle = "white";
-    ctx.fillRect(this.x,this.y,this.w,this.h);
-
-    if(this.isAttacking){
-      ctx.fillStyle = "red";
-      ctx.fillRect(
-        this.x + this.w,
-        this.y + 10,
-        this.attackRange,
-        10
-      );
-    }
-  }
-
-  takeDamage(dmg){
-    this.hp -= dmg;
-    if(this.hp <= 0){
-      this.hp = 0;
-      this.alive = false;
-    }
-  }
-
-  getAttackHitbox(){
-    if(!this.isAttacking) return null;
-
-    return {
-      x: this.x + this.w,
-      y: this.y + 10,
-      w: this.attackRange,
-      h: 10
-    };
+  render() {
+    this.ctx.save();
+    // 한지 배경에 대비되는 진한 먹색 실루엣 캐릭터
+    this.ctx.fillStyle = "#1c1917";
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    
+    // 검기 궤적 (진한 먹과 번짐 효과)
+    this.ctx.strokeStyle = "#292524";
+    this.ctx.lineWidth = 4;
+    this.ctx.beginPath();
+    this.ctx.arc(this.x + 30, this.y + 40, 55, 0, Math.PI);
+    this.ctx.stroke();
+    this.ctx.restore();
   }
 }
