a8ee9b77155fe8cd22f8ec26c198e52ea3715805
Yeongu <46391818+365creed@users.noreply.github.com>
2026-09-22 15:08:47 +0900

    Refactor drawSkyline function into StageSystem class

--- js/systems/stage.js
+++ js/systems/stage.js
@@ -1,16 +1,19 @@
-export function drawSkyline(ctx){
-  ctx.strokeStyle = "#111";
-  ctx.lineWidth = 2;
-  ctx.globalAlpha = 0.35;
+// stage.js
+export class StageSystem {
+  constructor(engine) {
+    this.engine = engine;
+    this.spawnTimer = 0;
+  }
 
-  const b = [
-    [20,420,50,200],
-    [90,380,60,240],
-    [170,410,45,210],
-    [230,360,70,260],
-    [310,400,40,220]
-  ];
-
-  b.forEach(v=>ctx.strokeRect(...v));
-  ctx.globalAlpha = 1;
+  update(dt) {
+    this.spawnTimer += dt;
+    // 4초마다 새로운 적 자동 출현
+    if (this.spawnTimer > 4) {
+      this.spawnTimer = 0;
+      if (this.engine.enemies.length < 4) {
+        const spawnX = Math.random() > 0.5 ? 1200 : 80;
+        this.engine.enemies.push(new this.engine.EnemyClass(this.engine.ctx, spawnX, 535));
+      }
+    }
+  }
 }
