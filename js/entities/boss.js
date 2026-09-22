f3580491c9d7a9c238cbff8ea51f0a389f88c2d0
Yeongu <46391818+365creed@users.noreply.github.com>
2026-09-22 15:08:14 +0900

    Update boss.js

--- js/entities/boss.js
+++ js/entities/boss.js
@@ -1,58 +1,14 @@
 export class Boss {
-  constructor(x,y){
+  constructor(ctx, x, y) {
+    this.ctx = ctx;
     this.x = x;
     this.y = y;
-
-    this.w = 120;
-    this.h = 120;
-
+    this.width = 90;
+    this.height = 130;
     this.hp = 300;
-    this.alive = true;
-
-    this.phase = 1;
-    this.damage = 20;
-
-    this.speed = 80;
+    this.isDead = false;
   }
 
-  update(dt, player){
-    if(!this.alive) return;
-
-    // 단순 이동 패턴
-    if(player.x < this.x){
-      this.x -= this.speed * dt;
-    }else{
-      this.x += this.speed * dt;
-    }
-
-    // 페이즈 변화
-    if(this.hp < 150){
-      this.phase = 2;
-      this.speed = 140;
-    }
-
-    if(this.hp <= 0){
-      this.alive = false;
-    }
-  }
-
-  draw(ctx){
-    if(!this.alive) return;
-
-    ctx.fillStyle = this.phase === 1 ? "purple" : "red";
-    ctx.fillRect(this.x,this.y,this.w,this.h);
-  }
-
-  takeDamage(dmg){
-    this.hp -= dmg;
-  }
-
-  getHitbox(){
-    return {
-      x:this.x,
-      y:this.y,
-      w:this.w,
-      h:this.h
-    };
-  }
+  update(dt) {}
+  render() {}
 }
